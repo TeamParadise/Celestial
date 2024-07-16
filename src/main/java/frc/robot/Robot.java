7eb70039e7224a24b6f485d18b0ed1f8deeb6738
Ashton A <uhdbits@duck.com>
2024-07-16 14:15:02 -0700

    Add dashboard layout
    Add a dashboard layout with tabs for pre-match (disabled) and during the match. Also includes many tuning tabs to allow easy tuning of the various subsystems.
    
    Signed-off-by: Ashton A <uhdbits@duck.com>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,6 +6,7 @@
 package frc.robot;
 
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Constants.Mode;
@@ -96,6 +97,10 @@ public class Robot extends LoggedRobot {
 
   @Override
   public void autonomousInit() {
+    // Switch to the "Match" tab on Shuffleboard/Elastic
+    Shuffleboard.selectTab("Match");
+
+    // Get autonomous command from the RobotContainer
     autonomousCommand = robotContainer.getAutonomousCommand();
 
     if (autonomousCommand != null) {
